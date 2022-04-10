4c4f9ea6393cdd799341d44d68733105e6f0ebb2
JeongUijeong <94631526+JeongUijeong@users.noreply.github.com>
2022-04-10 14:53:04 +0900

    [의정] token 추출 코드 추가 & response 수정 & 주석 추가

--- controllers/LikeController.js
+++ controllers/LikeController.js
@@ -1,35 +1,49 @@
-const LIKE = require('../models/').LIKE; //Model
+//Model
+const LIKE = require('../models/').LIKE;
+const USER = require('../models/').USER; 
 
 //찜설정or찜취소
 exports.Like = async function(req, res) {
     try {
-        await LIKE.findOne({where : {token: req.body.token, r_code: r_code}})
+        //요청 헤더 내의 authorization 헤더에서 토큰 추출
+        let token = req.headers.authorization.split('Bearer ')[1];
+        //토큰과 일치하는 user id 가져오기
+        let id = await USER.findOne({
+            attributes: ['id'], 
+            where : {token: token}
+        })
+        //해당 음식점을 찜했는지 확인
+        await LIKE.findOne({where : {id: id, r_code: r_code}})
         .then((data) => {
             if(data) { // 반환 데이터가 있다면 취소요청이 들어온 것
                 LIKE.destroy({
-                    where: {token: req.body.token, r_code: r_code}
+                    where: {id: id, r_code: r_code}
                 })
-                res.status(200).send({
+                res.status(204).json({
                     message: "찜이 취소됐습니다."
                 });
             }
-            else {
+            else { //반환 데이터가 없다면 새로이 찜을 하겠다는 것
                 LIKE.create({
-                    token : req.body.token, 
+                    id : id, 
                     r_code : req.body.r_code
                 })
                 .then((result) => {
-                    res.status(200).send('찜하셨습니다. ');
+                    res.status(201).json({
+                        message: '찜하셨습니다. '
+                    });
                 })
                 .catch((err) => {
-                    res.send(err);
+                    res.status(400).json({
+                        error: err
+                    });
                 });
             }      
         });
     } catch (error) {
         console.log(error);
-            res.status(400).send({
-                errorMessage: '찜 처리중 오류가 발생했습니다. '
+            res.status(400).json({
+                error: '찜 처리중 오류가 발생했습니다. '
             })
     }
 }
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,20 +5,22 @@ const LIKE = require('../models/').LIKE;
 
 
 const Views = '../views'
+
+//해시 암호화 내장 모듈
 const crypto = require('crypto');
-//아직 view에 대한 계획 X
-//router에서 받은 request에 대해 Model에서 받은 데이터 작업 수행 후 결과 View로 전달
 
 //회원가입
 exports.Signup = async function(req, res) {
     try {
+        //해시 암호화
         let salt_value = Math.round((new Date().valueOf() * Math.random())) + "";
         let hash_pw = crypto.createHash("sha512").update(req.body.pw + salt_value).digest("hex");
+        //id 중복확인 후 회원가입 진행
         await USER.findOne({where : {id: req.body.id}})
         .then((data) => {
             if(data) { // 반환 데이터가 있다면 이미 존재하는 id
-                res.status(400).send({
-                    message: "이미 존재하는 아이디입니다."
+                res.status(409).json({
+                    error: "이미 존재하는 아이디입니다."
                 });
             }
             else {
@@ -29,33 +31,42 @@ exports.Signup = async function(req, res) {
                     nickname : req.body.nickname
                 })
                 .then((result) => {
-                    res.status(200).send('회원가입 성공');
+                    res.status(201).json({
+                        message: '회원가입 성공'
+                    });
                 })
                 .catch((err) => {
-                    res.send(err);
+                    res.status(400).json({
+                        error: err
+                    });
                 });
             }      
         });
     } catch (error) {
         console.log(error);
-            res.status(400).send({
-                errorMessage: '회원가입 중 오류 발생했습니다. '
+            res.status(400).json({
+                error: '회원가입 중 오류 발생했습니다. '
             })
     }
 }
 
 exports.Mypage = async function(req, res) {
-    try {
+    try { 
+        //요청 헤더 내의 authorization 헤더에서 토큰 추출
+        let token = req.headers.authorization.split('Bearer ')[1];
+        //해당 user의 nickname 받아오기
         let userNickname = await USER.findOne({
             attributes: ['nickname'], 
-            where : {token: req.body.token}
+            where : {token: token}
         })
-        if(userInfo) {
+        if(userNickname) {
+            //user가 찜한 음식점의 r_code 받아오기
             let likeRestaurantList = await LIKE.findAll({
                 attributes: ['r_code'], 
-                where : {token: req.body.token}
+                where : {token: token}
             })
             let likeList = [];
+            //받은 r_code 리스트로 해당 음식점 정보 리스트 받아오기
             for (const key in likeRestaurantList){
                 restaurantInfo = await RESTAURANT.findOne({
                     where : {r_code: key}
@@ -66,23 +77,22 @@ exports.Mypage = async function(req, res) {
                         address: restaurantInfo.address,
                         star:restaurantInfo.stars,
                         options: {
-                         takeout: restaurantInfo.takeout,
-                         parking: restaurantInfo.parking,
+                            takeout: restaurantInfo.takeout,
+                            parking: restaurantInfo.parking,
                        }
                 })
             }
-            res.send({
-                status: 200, 
-                data: {
-                    nickname: userNickname, 
-                    like_list: likeList                  
-                }
+            res.status(200).json({ 
+                nickname: userNickname, 
+                like_list: likeList
             })
+        } else {
+
         }
     } catch (error) {
         console.log(error);
-            res.status(400).send({
-                errorMessage: '마이페이지 로딩중 오류가 발생했습니다. '
+            res.status(400).json({
+                error: '마이페이지 로딩중 오류가 발생했습니다. '
             })
     }
 }
