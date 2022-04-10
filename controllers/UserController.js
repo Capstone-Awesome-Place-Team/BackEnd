64ade50ef440ebe99935a7229123df4dd1ccdf30
fligger515 <fligger515@gmail.com>
2022-04-10 23:40:15 +0900

    [유빈] JWT 구현 전 로그인 구현 완료

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,6 @@
 const USER = require('../models/').USER; //Model
 const Views = '../views'
 const crypto = require('crypto');
-require("dotenv").config();
-const jwt = require('jsonwebtoken');
-
-//아직 view에 대한 계획 X
 //router에서 받은 request에 대해 Model에서 받은 데이터 작업 수행 후 결과 View로 전달
 
 //회원가입
@@ -49,47 +45,28 @@ exports.Signup = async function(req, res) {
 }
 
 //유빈 --- 로그인 구현
-const token = () => {
-  return {
-    access(id) {
-      return jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn : "30m",
-      });
-    }
-    refresh(id) {
-      return jwt.sign({id}, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn : "180 days",
-      });
-    }
-  }
-}
-
-exports.Signin = async function(req, res, next) {
+exports.Signin = async function(req, res) {
   try {
     let body = req.body;
     if(!body.id) {
-      res.status(500).json("존재하지 않는 아이디 입니다.");
+      res.status(500).send("존재하지 않는 아이디 입니다.");
       return;
     }
 
-    let result = await USER.findOne({where : {id : req.body.id} });
-    let db_pw = result.dataValues.pw;
+    let result = await USER.findOne({where : {id : req.body.id} })
+    let db_pw = result.dataValues.pw
     let salt = result.dataValues.salt;
-    let hash_pw = crypto.createHash("sha512").update(body.pw + salt).digest("hex"); //공부하고 전역 변수로 설정할 수 있는지 알아보기
+    let hash_pw = crypto.createHash("sha512").update(body.pw + salt).digest("hex");
+    //공부하고 전역 변수로 설정할 수 있는지 알아보기
 
     if (db_pw === hash_pw) {
-        res.json({
+        res.send({
             message : "로그인에 성공하였습니다",
-            status : '200',
-            data : { id : body.id },
-            jwt : { //authData
-              accessToken : token().access(req.query.id),
-              refreshToken : token().refresh(req.query.id),
-            }
+            status : 'success',
+            data : { id : body.id }
         }); //브라우저에 그대로 노출됨 *수정필요
     }
-    else { res.status(500).json({ message: "잘못된 비밀번호 입니다" }); }
-    next();
+    else { res.status(500).send({ message: "잘못된 비밀번호 입니다" }); }
   }
   catch (error) { console.error("알 수 없는 에러입니다:", error.message ) }
 }
