baa1c7e5a9cdb705993e355e4c9477b7c3d36018
fligger515 <fligger515@gmail.com>
2022-04-12 18:30:32 +0900

    [유빈] jwt토큰 생성 추가 및 main과 merge

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,25 +1,11 @@
 const USER = require('../models/').USER; //Model
 const Views = '../views'
 const crypto = require('crypto');
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+
 //아직 view에 대한 계획 X
 //router에서 받은 request에 대해 Model에서 받은 데이터 작업 수행 후 결과 View로 전달
-
-const createSalt = () =>
-    new Promise((resolve, reject) => {
-        crypto.randomBytes(64, (err, buf) => {
-            if (err) reject(err);
-            resolve(buf.toString('base64'));
-        });
-    });
-
-const createHashedPassword = (plainPassword) =>
-    new Promise(async (resolve, reject) => {
-        const salt = await createSalt();
-        crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-            if (err) reject(err);
-            resolve({ password: key.toString('base64'), salt });
-        });
-    });
 
 //회원가입
 exports.Signup = async function(req, res) {
@@ -35,18 +21,18 @@ exports.Signup = async function(req, res) {
             }
             else {
                 USER.create({
-                    id : req.body.id, 
-                    pw : hash_pw, 
-                    salt : salt_value, 
+                    id : req.body.id,
+                    pw : hash_pw,
+                    salt : salt_value,
                     nickname : req.body.nickname
                 })
                 .then((result) => {
-                    res.status(200).send('로그인 성공');
+                    res.status(200).send('회원가입 성공');
                 })
                 .catch((err) => {
                     res.send(err);
                 });
-            }      
+            }
         });
     } catch (error) {
         console.log(error);
@@ -55,3 +41,50 @@ exports.Signup = async function(req, res) {
             })
     }
 }
+
+//유빈 --- 로그인 구현
+const token = () => {
+  return {
+    access(id) {
+      return jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn : "30m",
+      });
+    }
+    refresh(id) {
+      return jwt.sign({id}, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn : "180 days",
+      });
+    }
+  }
+}
+
+exports.Signin = async function(req, res, next) {
+  try {
+    let body = req.body;
+    if(!body.id) {
+      res.status(500).json("존재하지 않는 아이디 입니다.");
+      return;
+    }
+
+    let result = await USER.findOne({where : {id : req.body.id} });
+    let db_pw = result.dataValues.pw;
+    let salt = result.dataValues.salt;
+    let hash_pw = crypto.createHash("sha512").update(body.pw + salt).digest("hex"); //공부하고 전역 변수로 설정할 수 있는지 알아보기
+
+    if (db_pw === hash_pw) {
+        res.json({
+            message : "로그인에 성공하였습니다",
+            status : '200',
+            data : { id : body.id },
+            jwt : { //authData
+              accessToken : token().access(req.query.id),
+              refreshToken : token().refresh(req.query.id),
+            }
+        }); //브라우저에 그대로 노출됨 *수정필요
+    }
+    else { res.status(500).json({ message: "잘못된 비밀번호 입니다" }); }
+    next();
+  }
+  catch (error) { console.error("알 수 없는 에러입니다:", error.message ) }
+}
+//---유빈
