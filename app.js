90a0893a4011da6ea1ecb0c92723052275141885
Pluto_ <81195613+Seonh0@users.noreply.github.com>
2022-04-30 01:49:44 +0900

    Seonho (#16)
    * pull main
    
    * [선호] 개인 정보 수정 구현
    
    * [선호] 개인 정보 수정 기능 코드 수정

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000; //포트 번호
-const router = require('./routes/routes')
-const cors = require('cors'); //npm i cors : cors 미들웨어 설치
+const router = require("./routes/routes");
+const cors = require("cors"); //npm i cors : cors 미들웨어 설치
 
 //테스트용 view를 실행시키기 위한 코드들
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const hostname = '127.0.0.1';
-const path = require('path')
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const hostname = "127.0.0.1";
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //cors 문제 해결
-app.use(cors({
-    origin: '*', //출처 허용
-    credential: 'true' //사용자 인증이 필요한 리소스 접근
-}))
+app.use(
+  cors({
+    origin: "*", //출처 허용
+    credential: "true", //사용자 인증이 필요한 리소스 접근
+  })
+);
 
 //Sequelize 확인
-const { sequelize } = require('./models');
-sequelize.sync({ force : false})
-	.then(() => {
-		console.log('연결성공');
-	})
-	.catch((err) => {
-		console.error(`연결실패 - ${err}`);
-	});
+const { sequelize } = require("./models");
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("연결성공");
+  })
+  .catch((err) => {
+    console.error(`연결실패 - ${err}`);
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/signup', (req, res) => {
-	res.render('signup');
-})
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-app.get('/signin', (req, res) => {
-	res.render('signin');
-})
+app.get("/signin", (req, res) => {
+  res.render("signin");
+});
 
-app.use('/', router)
+app.use("/", router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //포트 응답 확인
