2f80b403c78e92313ba42b3e640e85367f2a28b6
Seonh0 <dkdlatjsh@gmail.com>
2022-05-15 03:10:34 +0900

    [선호] 레스토랑 데이터 DB저장 코드 추가

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 80; //포트 번호
 const router = require("./routes/routes");
 const cors = require("cors"); //npm i cors : cors 미들웨어 설치
-
+const getConnection = require("./controllers/DBController");
 //테스트용 view를 실행시키기 위한 코드들
 const http = require("http");
 const server = http.createServer(app);
@@ -47,3 +47,121 @@ app.get("/signin", (req, res) => {
 app.use("/", router);
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //포트 응답 확인
+
+// const jsonFile = fs.readFileSync("./data.json", "utf8");
+// const jsonData = JSON.parse(jsonFile);
+// getConnection((conn) => {
+//   for (let i = 0; i < 250; i++) {
+//     conn.query("insert into RESTAURANT value (?,?,?,?,?,?,?,?,?)", [
+//       i,
+//       jsonData.data[i].img,
+//       jsonData.data[i].restaurant_name,
+//       jsonData.data[i].address,
+//       jsonData.data[i].tag,
+//       jsonData.data[i].price,
+//       jsonData.data[i].star,
+//       jsonData.data[i].options.parking,
+//       jsonData.data[i].options.takeout,
+//     ]);
+//   }
+//   console.log("connection");
+//   conn.release();
+// });
+
+// pool
+//   .getConnection()
+//   .then((conn) => {
+//     conn
+//       .query("SELECT 1 as val")
+//       .then((data) => {
+//         for (let i = 0; i < 250; i++) {
+//           conn.query("insert into RESTAURANT value (?,?,?,?,?,?,?,?,?)", [
+//             i,
+//             jsonData.data[i].img,
+//             jsonData.data[i].restaurant_name,
+//             jsonData.data[i].address,
+//             jsonData.data[i].tag,
+//             jsonData.data[i].price,
+//             jsonData.data[i].star,
+//             jsonData.data[i].options.parking,
+//             jsonData.data[i].options.takeout,
+//           ]);
+//         }
+//       })
+//       .then((res) => {
+//         conn.release();
+//       })
+//       .catch((err) => {
+//         conn.release();
+//         console.log(err);
+//       });
+//   })
+//   .catch((err) => {
+//     console.log("not connected");
+//   });
+const configData = JSON.parse(fs.readFileSync("./config/config.json", "utf8"));
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+  username: configData.development.username,
+  password: configData.development.password,
+  database: configData.development.database,
+  host: configData.development.host,
+  dialect: configData.development.dialect,
+});
+
+let pool = mysql.createPool({
+  username: configData.development.username,
+  password: configData.development.password,
+  database: configData.development.database,
+  host: configData.development.host,
+  dialect: configData.development.dialect,
+});
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+
+//   console.log("connected as id " + connection.threadId);
+// });
+
+const jsonFile = fs.readFileSync("./data.json", "utf8");
+const jsonData = JSON.parse(jsonFile);
+pool.getConnection(function (err, connection) {
+  if (err) throw err; // not connected!
+
+  // Use the connection
+  for (let i = 0; i < 250; i++) {
+    connection.query(
+      "insert into RESTAURANT value (?,?,?,?,?,?,?,?,?)",
+      [
+        i,
+        jsonData.data[i].img,
+        jsonData.data[i].restaurant_name,
+        jsonData.data[i].address,
+        jsonData.data[i].tag,
+        jsonData.data[i].price,
+        jsonData.data[i].star,
+        jsonData.data[i].options.parking,
+        jsonData.data[i].options.takeout,
+      ],
+      function (error, results, fields) {
+        if (error) throw error;
+        // Neat!
+      }
+    );
+  }
+  // connection.query(
+  //   "SELECT something FROM sometable",
+  //   function (error, results, fields) {
+  //     // When done with the connection, release it.
+  //     connection.release();
+
+  //     // Handle error after the release.
+  //     if (error) throw error;
+
+  //     // Don't use the connection here, it has been returned to the pool.
+  //   }
+  // );
+});
